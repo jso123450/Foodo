3e4b5c975fdccff699999a9c0dd4321b39455601
Javis <wujavis@gmail.com>
2015-11-12 23:47:36 -0500

    embedded map good

--- google.py
+++ google.py
@@ -1,13 +1,13 @@
 import urllib2
 import json
 
-key="AIzaSyCeavvXLPxaal8kb7SFnzYpShJ37vNCjQg"
-
+dKey="AIzaSyCeavvXLPxaal8kb7SFnzYpShJ37vNCjQg"
+mKey="AIzaSyBSVLrtt6Hb6MDqyXdAHunxuYp0njyh2Dg"
 def directions(start,end,mode):
     origin = start
     destination = end
     mode = mode
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=%s&key=%s" %(origin, destination, mode, key)
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=%s&key=%s" %(origin, destination, mode, dKey)
     print url
     request = urllib2.urlopen(url)
     result = request.read()
@@ -15,4 +15,15 @@ def directions(start,end,mode):
     print r.keys()
     step0= r['routes'][0]['legs'][0]['steps'][0]
     print step0['html_instructions']
-print directions("Brooklyn_Bridge","Prospect_Park,Brooklyn","driving")
+#print directions("Brooklyn_Bridge","Prospect_Park,Brooklyn","driving")
+
+#use as source of iframe for use on website
+def map(start,end,mode):
+    origin = start
+    destination = end
+    mode = mode
+    url="https://www.google.com/maps/embed/v1/directions?key=%s&origin=%s&destination=%s&mode=%s" %(mKey, origin, destination, mode)
+    return url
+
+print map("Brooklyn_Bridge","Prospect_Park,Brooklyn","driving")
+    
