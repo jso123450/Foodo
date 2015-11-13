d4cc7373e0f18ebecf581622e7f6aab3cad9d899
Javis <wujavis@gmail.com>
2015-11-13 01:00:44 -0500

    directions working mostly

--- google.py
+++ google.py
@@ -3,19 +3,28 @@ import json
 
 dKey="AIzaSyCeavvXLPxaal8kb7SFnzYpShJ37vNCjQg"
 mKey="AIzaSyBSVLrtt6Hb6MDqyXdAHunxuYp0njyh2Dg"
-def directions(start,end,mode):
+def routes(start,end,mode):
     origin = start
     destination = end
     mode = mode
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=%s&key=%s" %(origin, destination, mode, dKey)
-    print url
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&mode=%s&alternatives=true&key=%s" %(origin, destination, mode, dKey)
     request = urllib2.urlopen(url)
     result = request.read()
     r=json.loads(result)
-    print r.keys()
-    step0= r['routes'][0]['legs'][0]['steps'][0]
-    print step0['html_instructions']
-#print directions("Brooklyn_Bridge","Prospect_Park,Brooklyn","driving")
+    return r['routes']
+
+#three routes to choose
+def routeInstructions(start,end,mode,routeNum):
+    result=""
+    allRoutes = routes(start,end,mode)
+    routeSteps = allRoutes[routeNum]['legs'][0]['steps']
+    for x in routeSteps:
+        result=result+x['html_instructions'].encode('utf8')+"<br>"
+    return result
+
+print routeInstructions("Brooklyn+Bridge","Prospect+Park,Brooklyn","driving",2)
+#note when returning this route long dash isn't properly returned. returns questions marks. something to do with the encoding
+
 
 #use as source of iframe for use on website
 def map(start,end,mode):
@@ -25,5 +34,6 @@ def map(start,end,mode):
     url="https://www.google.com/maps/embed/v1/directions?key=%s&origin=%s&destination=%s&mode=%s" %(mKey, origin, destination, mode)
     return url
 
-print map("Brooklyn_Bridge","Prospect_Park,Brooklyn","driving")
+#print map("1301+Dahill+Road,Brooklyn","Stuyvesant+High+School,New+York","driving")
+print map("Brooklyn+Bridge","Prospect+Park,Brooklyn","driving")
     
