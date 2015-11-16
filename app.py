a49d8bac8d0883df16a0ba8d3ee6d6f24758d973
Johnny So <jso123450@gmail.com>
2015-11-15 22:38:45 -0500

    Polished up some stuff

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 @app.route("/index",methods=['GET','POST'])
 @app.route("/home",methods=['GET','POST'])
 def index():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template("index.html")
     else:
         button = request.form['button']
         if button == "Set Location and Transit Method":
+            # set session keys to store transit mode & location
             prefTrans = request.form['transMethod']
             session['modeTrans'] = prefTrans
             userLocation = request.form['userLocation']
@@ -19,13 +20,16 @@ def index():
             return render_template("index.html")
         elif button == "Search":
             if session.has_key('userLocation'):
-                if session['userLocation']=="":
-                    error="No location given"
-                    return render_template("index.html",error=error)
+                # safety
                 food = request.form['food']
-                if food=="":
-                    error="You're not craving anything."
-                    return render_template("index.html",error=error)
+                location = session['userLocation']
+                if location == "" or food == "":
+                    if location == "":
+                        error = "Where do you want to eat? The North Pole?"
+                        return render_template("index.html",error=error)
+                    if food == "":
+                        error = "You're not craving anything."
+                        return render_template("index.html",error=error)
                 location = session['userLocation']
                 return redirect ('/search/%s/%s' %(location,food))
             else:
@@ -33,17 +37,14 @@ def index():
                 return render_template("index.html",error=error)
     return render_template("index.html")
 
-
 @app.route("/search",methods=['GET','POST'])
 @app.route("/search/",methods=['GET','POST'])
 @app.route("/search/<location>/",methods=['GET','POST'])
 @app.route("/search/<location>/<term>",methods=['GET','POST'])
-def search(location="NY",term="restaurants"):
-    if request.method=='GET':
-        restaurantDic = yelp.getRestaurants(term,location)
-        return render_template("search.html",restaurants=restaurantDic)
-    else:
-        return redirect(url_for(directions(request.form['placeName'])))
+def search(location="New York",term="restaurants"):
+    # default location is New York, default term is restaurants
+    restaurantDic = yelp.getRestaurants(term,location)
+    return render_template("search.html",restaurants=restaurantDic)
 
 @app.route("/directions",methods=['GET','POST'])
 def directions():
@@ -52,20 +53,20 @@ def directions():
     travelMethod = session['modeTrans']
     place = session['restaurantAddress']
     route1 = google.routeInstructions(start,place,travelMethod,0)
+    # in case there are no alternatives
     try:
         route2 = google.routeInstructions(start,place,travelMethod,1)
     except:
-        route2="No alternative"
+        route2 = "No alternative"
     try:
         route3 = google.routeInstructions(start,place,travelMethod,1)
     except:
         route3 ="No alternative"
-        
-    theMap  = google.mapDirections(start,place,travelMethod)
+    theMap = google.mapDirections(start,place,travelMethod)
     return render_template("directions.html",route1=route1,route2=route2, 
                            route3=route3,mapSrc = theMap)
 
 if __name__ == "__main__":
     app.debug = True
-    app.secret_key="hoyinhoooo"
+    app.secret_key = "appapp"
     app.run(host='0.0.0.0',port=8000)
